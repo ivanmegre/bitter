8612c3137ea4f679231cc4478423f0705e0cc8c9
Ivan Souto <ivan.souto@raise.com>
2025-03-22 20:07:48 -0300

    finalize setting up main init

--- bitter/lib/main.dart
+++ bitter/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bitter/common/theme/theme.dart';
 import 'package:bitter/presentation/onboarding/onboarding_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.dark,
+      supportedLocales: AppLocalizations.supportedLocales,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
       home: OnboardingPage(),
     );
   }
