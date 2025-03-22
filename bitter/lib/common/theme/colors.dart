64073f84d09535f7230a1c99c10e6f4d18ccbc84
Ivan Souto <ivan.souto@raise.com>
2025-03-22 17:30:12 -0300

    add base colors and theme

---
+++ bitter/lib/common/theme/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  // Magenta variatons
+  static const superMagenta = Color(0xFF21121A);
+  static const magenta = Color(0xFF472436);
+  static const lightMagenta = Color(0xFF950F4B);
+  static const lightPink = Color(0xFFC991AD);
+
+  static const mustard = Color(0xFFF5AB00);
+  static const darkBlue = Color(0xFF011638);
+  static const lightGreen = Color(0xFFDFF8EB);
+  static const darkGreen = Color(0xFF214E34);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0x00000000);
+  static const softBlack = Color(0xFF171411);
+  static const lightGrey = Color(0xFFB5B3B1);
+}
