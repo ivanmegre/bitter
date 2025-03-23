915a3c41baa89e63d0b08ea108b2b4bf27c26ec2
Ivan Souto <ivan.souto@raise.com>
2025-03-23 18:31:49 -0300

    add onboarding page

--- bitter/lib/presentation/onboarding/onboarding_page.dart
+++ bitter/lib/presentation/onboarding/onboarding_page.dart
@@ -1,3 +1,9 @@
+import 'package:bitter/common/extensions/context.dart';
+import 'package:bitter/common/theme/assets.dart';
+import 'package:bitter/common/theme/colors.dart';
+import 'package:bitter/common/theme/fonts.dart';
+import 'package:bitter/presentation/widgets/button.dart';
+import 'package:bitter/presentation/widgets/spacer.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingPage extends StatelessWidget {
@@ -5,6 +11,49 @@ class OnboardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: const AssetImage(Images.onboardingBackground),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(ColorPalette.softBlack, BlendMode.darken),
+          ),
+        ),
+        child: Center(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  context.localization.onboardingPage_appBarTitleCopy,
+                  style: Fonts.noto.title.boldLarge,
+                ),
+                Spacer(),
+                Text(
+                  context.localization.onboardingPage_bodyMainCopy,
+                  textAlign: TextAlign.center,
+                  style: Fonts.noto.title.extraBoldLarge,
+                ),
+                spacer(size: 8),
+                Text(
+                  context.localization.onboardingPage_bodySubtextCopy,
+                  textAlign: TextAlign.center,
+                  style: Fonts.noto.body.regularSmall,
+                ),
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Button.onboarding(
+                    label: context.localization.onboardingPage_buttonCopy,
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
